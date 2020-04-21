cbc9209149e8cd6dcc8ae217a6bfcb3d82aa3f5d
IlyaPolonskiy <mr.polonskiy96@mail.ru>
2020-04-21 14:26:28 +0300

    Add icon and button "HELLO"

---
+++ app/src/main/java/com/ipolo/androidpolonskiy/SplashScreenActivity.java
@@ -0,0 +1,20 @@
+package com.ipolo.androidpolonskiy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+    public void onClickGoToHello(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
